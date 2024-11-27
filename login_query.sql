fc40e021de6ef322ae022d341e002ba979cf070a
Ujjwal34-ui <ujjwalshrivastav8@gmail.com>
2024-11-27 22:31:20 +0545

    login_query.sql

--- login_query.sql
+++ login_query.sql
@@ -2,23 +2,27 @@ use sikshalaya
 SELECT user_id, username, role 
 FROM Users 
 WHERE username = '' 
+   AND email = ''
   AND password = SHA2('', 256) 
   AND role = 'Admin';
 
 SELECT user_id, username, role 
 FROM Users 
 WHERE username = '' 
+   AND email = ''
   AND password = SHA2('', 256) 
   AND role = 'Teacher';
 
 SELECT user_id, username, role 
 FROM Users 
 WHERE username = '' 
+   AND email = ''
   AND password = SHA2('', 256) 
   AND role = 'Student';
 
 SELECT user_id, username, role 
 FROM Users 
 WHERE username = '' 
+  AND email = ''
   AND password = SHA2('', 256) 
   AND role = 'Guard';
